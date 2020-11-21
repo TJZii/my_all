78d11248e713c9c59b2f37211f4deb6d5bdabb5f
TJZii <tjhaweszii@gmail.com>
2020-11-21 02:48:53 -0800

    commit tree 92650848fe4b5f63e1db6afb686fb0f4931eee60

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+    z = 0
+    return_vals = []
+    while z < collection.length
+        return_vals << yield(collection[z])
+        z += 1
+    end
 
+    if return_vals.include?(false)
+        false
+    else
+        true
+    end
 end
